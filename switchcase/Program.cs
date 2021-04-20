6e400e6ffcaa61ae298be597690c7b06eaf8ab88
arnoldo <Mauri@LAPTOP-0Q9ADC5L>
2021-04-20 14:40:11 -0700

    CommitInt

--- switchcase/Program.cs
+++ switchcase/Program.cs
@@ -19,27 +19,29 @@ namespace switchcase
              "\n5.- Salir");
 
             String s1 = null;
+            int i1 = 0;
             s1 = Console.ReadLine();
+            i1 = Convert.ToInt32(Console.ReadLine());
 
-            switch (s1)
+            switch (i1)
             {
-                case "1":
+                case 1:
                     Console.WriteLine("Seleccionaste suma");
                     break;
 
-                case "2":
+                case 2:
                     Console.WriteLine("Seleccionaste resta");
                     break;
 
-                case "3":
+                case 3:
                     Console.WriteLine("Seleccionaste multiplicacion");
                     break;
 
-                case "4":
+                case 4:
                     Console.WriteLine("Seleccionaste division");
                     break;
 
-                case "5":
+                case 5:
                     Console.WriteLine("Seleccionaste salir ");
                     break;
                 default:
